bdb77471ac3f2e23ee0b94564a1192ed33f61fb6
EndrII <EndrIIMail@gmail.com>
2020-08-06 14:14:29 +0300

    fix warnings

--- params.cpp
+++ params.cpp
@@ -161,7 +161,7 @@ bool Params::writeLoginFile(const QString &log, VerboseLvl vLvl) {
         if (logFile.open(QIODevice::WriteOnly | QIODevice::Append | QIODevice::Text)) {
 
             QTextStream stream(&logFile);
-            stream << timeString() <<"| " << QString::fromStdString(lvlToString(vLvl)) + ": " + log << endl;
+            stream << timeString() <<"| " << QString::fromStdString(lvlToString(vLvl)) + ": " + log << Qt::endl;
             logFile.close();
         } else {
             return false;
