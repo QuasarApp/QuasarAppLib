2d2e1a9a5f07713ccab06f1b32a4b35e961c1a0c
EndrII <EndrIIMail@gmail.com>
2022-07-24 13:44:52 +0300

    added new interface for working with human readably strings

---
+++ humanreadableobject.h
@@ -0,0 +1,38 @@
+//#
+//# Copyright (C) 2022-2022 QuasarApp.
+//# Distributed under the GPLv3 software license, see the accompanying
+//# Everyone is permitted to copy and distribute verbatim copies
+//# of this license document, but changing it is not allowed.
+//#
+
+#ifndef HUMANREADABLEOBJECT_H
+#define HUMANREADABLEOBJECT_H
+
+#include <QString>
+
+namespace QuasarAppUtils {
+
+/**
+ * @brief The HumanReadableObject interface This is simple class  that add one virtula method toString.
+ * All childs object should be override this method.
+ */
+class HumanReadableObject
+{
+public:
+    /**
+     * @brief toString This method convert this object to human readable string.
+     * @return human readable string of this object.
+     */
+    virtual QString toString() const = 0;
+
+protected:
+    HumanReadableObject();
+};
+
+/**
+ * @brief HRO This is short abriviature of the HumanReadableObject class.
+ */
+typedef HumanReadableObject HRO;
+
+}
+#endif // HUMANREADABLEOBJECT_H
