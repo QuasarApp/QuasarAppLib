81316a23d3a80b62ebeb4c1e35f21f3b9f43174c
Andrei Yankovich <EndrIIMail@gmail.com>
2022-07-26 13:07:39 +0300

    Update qaglobalutils.cpp

--- qaglobalutils.cpp
+++ qaglobalutils.cpp
@@ -15,7 +15,7 @@ void gen(int size, QByteArray &result) {
         result.insert(0, reinterpret_cast<char*>(&random), sizeof(random));
     }
 }
-
+// to do the random functions should be generate 4 bit number any wher exept last number
 void randomArray(int size, QByteArray &result) {
     if (size % sizeof(unsigned long long) == 0) {
         gen<unsigned long long>(size, result);
