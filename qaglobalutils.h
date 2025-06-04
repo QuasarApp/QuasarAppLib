e67fa0438648eb882e0dab69442384fabc713c5e
EndrII <EndrIIMail@gmail.com>
2025-06-04 11:14:54 +0200

    added qhash utils

--- qaglobalutils.h
+++ qaglobalutils.h
@@ -156,4 +156,68 @@ uint8_t static_type_hash_8(T& object) noexcept {
 */
 void QUASARAPPSHARED_EXPORT randomArray(int size, QByteArray &result);
 
+
+/*!
+ * \brief Simplified subtraction operation for QHash.
+ *
+ * This function provides a convenient way to remove elements from one `QHash`
+ * based on keys present in another `QHash`. It's designed to
+ * simplify common `QHash` operations.
+ *
+ * The function modifies the `left` QHash by removing all key-value pairs
+ * whose keys are present in the `right` QHash.
+ *
+ * If `left` and `right` share the same internal data (i.e., `left.isSharedWith(right)`
+ * returns true), the `left` hash is cleared entirely to prevent
+ * unpredictable behavior or unintended modifications to the shared data.
+ * Otherwise, it iterates through `right` and removes corresponding keys from `left`.
+ *
+ * \tparam QHashType The type of the QHash (e.g., QHash<QString, int>).
+ * \param left A reference to the QHash to be modified. Elements present in `right`
+ * will be removed from this hash.
+ * \param right A constant reference to the QHash containing keys to be removed from `left`.
+ * \return A reference to the modified `left` QHash.
+ */
+template <class QHashType>
+QHashType &subtract(QHashType &left, const QHashType& right) {
+    if (left.isSharedWith(right)) {
+        left.clear();
+    } else {
+        for (auto&& it =  right.begin(); it != right.end(); ++it)
+            left.remove(it.key());
+    }
+    return left;
+}
+
+/*!
+ * \brief Simplified union operation for QHash.
+ *
+ * This function provides a convenient way to unite the elements of two `QHash` objects,
+ * adding all elements from one to the other. It's designed to
+ * simplify common `QHash` operations.
+ *
+ * The function modifies the `left` QHash by inserting all key-value pairs
+ * from the `right` QHash. If a key from `right` already exists in `left`,
+ * its corresponding value in `left` will be updated with the value from `right`.
+ *
+ * This operation is skipped if `left` and `right` share the same internal data
+ * (i.e., `left.isSharedWith(right)` returns true) to avoid self-modification
+ * or infinite loops in cases of shared data.
+ *
+ * \tparam QHashType The type of the QHash (e.g., QHash<QString, int>).
+ * \param left A reference to the QHash to be modified. Elements from `right`
+ * will be inserted into this hash.
+ * \param right A constant reference to the QHash whose elements will be added to `left`.
+ * \return A reference to the modified `left` QHash.
+ */
+template <class QHashType>
+QHashType &unite(QHashType &left, const QHashType& right) {
+    if (!left.isSharedWith(right)) {
+        for (auto&& it =  right.begin(); it != right.end(); ++it)
+            left.insert(it.key(), it.value());
+    }
+    return left;
+}
+
+
 #endif // GLOBAL_H
